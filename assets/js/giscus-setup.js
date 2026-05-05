a2ffc44b5d5ea5723a6b90b6fd3ad07571cfdcf6
Rahul-Kumar-Kundu <126976757+Rahul-Kumar-Kundu@users.noreply.github.com>
2026-05-05 21:09:17 +0000

    Deploying to gh-pages from @ Rahul-Kumar-Kundu/Rahul-Kumar-Kundu.github.io@95add0195d5f0ffdcd6ffd09071d06b07b1345df 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Rahul-Kumar-Kundu/rahulkumarkundu.github.io",
+    "data-repo": "Rahul-Kumar-Kundu/Rahul-Kumar-Kundu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
