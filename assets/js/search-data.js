ab0b41af0bedfb5ad32ca4406bf0f5fe9de63a2e
Rahul-Kumar-Kundu <126976757+Rahul-Kumar-Kundu@users.noreply.github.com>
2026-08-04 08:35:45 +0000

    Deploying to gh-pages from @ Rahul-Kumar-Kundu/Rahul-Kumar-Kundu.github.io@13590b62c3ce6cd3af5bdb788c9056d947f03c05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "Publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
